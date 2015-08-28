894273d23dce65cb681f0a0bea272500954401a2
sbcoba <sbcoba@gmail.com>
2015-08-28 12:19:17 +0900

    Spring Boot mybatis default config sample

---
+++ spring-boot-mybatis-sample-default/src/main/java/sample/springboot/mybatis/defaultconfig/service/Member.java
@@ -0,0 +1,37 @@
+package sample.springboot.mybatis.defaultconfig.service;
+
+/**
+ * Sample DTO
+ * 
+ * @author sbcoba
+ *
+ */
+public class Member {
+	private Long id;
+	private String name;
+	private String phone;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", phone=" + phone + "]";
+	}
+}
